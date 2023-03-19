929d26f726e2619ad1cfa2b343491876908ee467
Hersh Bhargava <hbhargava@Hades.local>
2023-03-19 16:01:09 -0700

    Update chrome user agent to prevent pubmed 403 error

--- pubmed_sieve.py
+++ pubmed_sieve.py
@@ -379,6 +379,11 @@ def gen_pubmed_rss_link_for_query(feed_name: str, query_string: str):
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--window-size=1920,1080")
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        chrome_options.add_argument(f'user-agent={user_agent}')
+        chrome_options.add_argument('--allow-running-insecure-content')
+        
         driver = webdriver.Chrome('chromedriver',options=chrome_options)
 
         # Navigate to the PubMed website
